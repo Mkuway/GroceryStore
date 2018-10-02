a417f3b2df80cd1df76156e358e64e5b33c8a90b
Mkuway <mkuway@mail.csuchico.edu>
2018-10-01 17:41:33 -0700

    User inputs 5 grocery items that are listed.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//author: Mathew G. Kuway
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,38 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string Itemname;
+do
+{
+  
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+    if(input == 'a' || input == 'A')
+    {
+    cout<<"What is the item?"<<endl;
+    cin>> Itemname;
+    
+    if(numItems<5)
+    {
+    list[numItems]=Itemname;
+            numItems++;
+    }
+    if(numItems == 5)
+    {
+            cout<<"You'll need a bigger list!"<<endl;
 
-  return 0;
+    }
+    }
+}while(input != 'q' && input != 'Q');
+    cout<<"\n==ITEMS TO BUY=="<<endl;
+ for(int index = 0; index < 5; index++)
+    {
+            cout<<index + 1<<" "<<list[index]<<endl;
+    }
+  
+return 0;
 }
